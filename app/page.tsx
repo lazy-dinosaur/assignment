e7e8f782ad02aae17abc95215f22acdccfb57567
lazydino <woohs0130@naver.com>
2025-05-21 23:13:02 +0900

    Feat: 바뀐 훅 적용 및 테이블 추가중

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 import Image from "next/image";
 import { useRef } from "react";
-import { firstBubble, secondBubble } from "./libs";
+import { firstBubble, getName, getTableDimension, secondBubble } from "./libs";
 import { USER_NAME } from "./constants";
 import Bubble from "./components/bubble";
-import useTextSize from "./hooks/useTextSize";
+import useGetpx from "./hooks/useGetpx";
 
 export default function Home() {
   const imageRef = useRef<HTMLImageElement>(null);
-  const fontSize = useTextSize(imageRef, 18.5);
+  const { fontSize, tableThickBorder } = useGetpx(imageRef, {
+    fontSize: 18,
+    tableThickBorder: 3,
+  });
+  const { tableTop, tableWidth, tableHeight, tableLeft } = getTableDimension();
 
   return (
     <div className="max-w-md mx-auto bg-[#F3F2EF] w-full">
@@ -25,15 +29,27 @@ export default function Home() {
         {fontSize && (
           <>
             <Bubble
-              text={`이제 본격적으로 ${USER_NAME}님의 사주팔자를 분석해볼 차례네요.`}
+              text={`이제 본격적으로 ${getName(USER_NAME)}님의 사주팔자를 분석해볼 차례네요.`}
               textSize={fontSize}
               bubble={firstBubble}
             />
             <Bubble
-              text={`제가 ${USER_NAME}님의 사주를 보기 쉽게 표로 정리했어요`}
+              text={`제가 ${getName(USER_NAME)}님의 사주를 보기 쉽게 표로 정리했어요`}
               textSize={fontSize}
               bubble={secondBubble}
             />
+            <div
+              className={`absolute bg-[#F5F3EC] shadow-lg border-[#1B2F49]`}
+              style={{
+                top: `${tableTop}%`,
+                width: `${tableWidth}%`,
+                height: `${tableHeight}%`,
+                left: `${tableLeft}%`,
+                borderWidth: tableThickBorder,
+              }}
+            >
+              <div className={`flex w-full h-full`} style={{}}></div>
+            </div>
           </>
         )}
       </div>
