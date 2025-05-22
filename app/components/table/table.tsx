1d95a51538368a91ab780d09f69529efc62e7ea2
lazydino <woohs0130@naver.com>
2025-05-22 12:17:33 +0900

    Feat: 테이블의 제목 텍스트 추가중

--- app/components/table/table.tsx
+++ app/components/table/table.tsx
@@ -1,10 +1,8 @@
+import { getTableDimension } from "@/app/libs";
 import Borders from "./border-component";
 import Patterns from "./patterns-component";
+import { USER_NAME } from "@/app/constants";
 type TableProps = {
-  tableTop: number;
-  tableWidth: number;
-  tableHeight: number;
-  tableLeft: number;
   tableThickBorder: number;
   tableThinBorder: number;
   firstSvgTop: number;
@@ -12,19 +10,17 @@ type TableProps = {
   gap: number;
 };
 export default function Table({
-  tableTop,
-  tableWidth,
-  tableHeight,
-  tableLeft,
   tableThickBorder,
   tableThinBorder,
   firstSvgTop,
   secondSvgTop,
   gap,
 }: TableProps) {
+  const { tableTop, tableWidth, tableHeight, tableLeft } = getTableDimension();
+
   return (
     <div
-      className={`absolute bg-[#F5F3EC] shadow-lg border-[#1B2F49] flex items-center justify-center`}
+      className={`absolute bg-[#F5F3EC] shadow-lg border-[#1B2F49] flex justify-center`}
       style={{
         top: `${tableTop}%`,
         width: `${tableWidth}%`,
@@ -35,6 +31,10 @@ export default function Table({
     >
       <Borders gap={gap} tableThinBorder={tableThinBorder} />
       <Patterns gap={gap} firstTop={firstSvgTop} secondTop={secondSvgTop} />
+      <div className={`text-[#424242]`} style={{}}>
+        <h4>{USER_NAME}님의 사주</h4>
+        <h2></h2>
+      </div>
     </div>
   );
 }
